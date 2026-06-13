57b9e9d1bdf77ce6e866ab76982afd5f9233a941
ranveergupta635-devops <ranveergupta635@gmail.com>
2026-06-13 12:41:46 +0530

    initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+subscription_id = "64642b7c-dbcc-403f-aed7-cc959069fa72"
+rg_name          = "demo-rg2"
+location         = "Central India"
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "rg_name" {
+  description = "Resource Group Name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
